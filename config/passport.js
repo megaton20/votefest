08044a99e58bc1b884ecea455f5a27447262d10d
megaton20 <ruralaidinitiativengo.com>
2026-01-09 17:35:39 +0100

    live env 3

--- config/passport.js
+++ config/passport.js
@@ -2,7 +2,6 @@ const passport = require('passport');
 // const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const LocalStrategy = require('passport-local').Strategy;
 const bcrypt = require('bcryptjs');
-require('dotenv').config();
 const User = require("../models/User");
 
 
